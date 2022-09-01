5aa868d7cea6282da08d906f89c9f6af59a16a5d
mbaray <baraymasha@gmail.com>
2022-09-01 10:55:36 +0300

    add layouts

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title')
+    title
+@endsection
+
+@section('header')
+    header
+@endsection
+
+@section('content')
+    <h1>О блоге</h1>
+    <p>Эксперименты с Laravel на Хекслете</p>
+@endsection
